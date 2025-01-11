345c47d355f71353bf85bc423c05074219afd4f0
Vineet Verma <vineetverma321@gmail.com>
2025-01-12 00:35:13 +0530

    Exercise 2.6

--- P20620/src/App.jsx
+++ P20620/src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [newName, setNewName] = useState('');
+
+  const handleNewName = (e) => {
+    setNewName(e.target.value);
+  };
+
+  const addNewName = (e) => {
+    e.preventDefault();
+    const newNameObj = {
+      name: newName,
+    };
+    setPersons(persons.concat(newNameObj));
+    setNewName('');
+  };
+
+  const Person = ({ perObj }) => {
+    return <div>{perObj.name}</div>;
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNewName}>
+        <div>
+          name: <input value={newName} onChange={handleNewName} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        // <div>{person.name}</div>
+        <Person perObj={person} key={person.name} />
+      ))}
+    </div>
+  );
+};
 
-export default App
+export default App;
